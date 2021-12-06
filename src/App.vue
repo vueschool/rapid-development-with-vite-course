58b8f3bf508d726d4226f25292fa3d3a1e21118b
Daniel Kelly <me@danielkelly.io>
2021-12-06 16:23:03 -0600

    typescript and vite

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "./components/HelloWorld.vue";
 import styles from "./assets/example.module.css";
-import { developerMood } from "./data.json";
+const developerMood: string = "Thrilled for Typescript!";
 console.log(developerMood);
 </script>
 
