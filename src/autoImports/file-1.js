912bdc2fad853c485aa70a98c988fc7352d862f2
Daniel Kelly <me@danielkelly.io>
2021-12-06 15:22:05 -0600

    glob imports in vite

---
+++ src/autoImports/file-1.js
@@ -0,0 +1,1 @@
+console.log("hello file 1");
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 
+const autoImportedModules = import.meta.globEager("./autoImports/*.js");
+
 createApp(App).mount("#app");
