bbd062fce9e3d7fa1dcf3cbc1531301861467f0e
Daniel Kelly <me@danielkelly.io>
2021-12-06 10:11:49 -0600

    css modules and vite

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "./components/HelloWorld.vue";
+import styles from "./assets/example.module.css";
+console.log(styles);
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 :class="styles.example">Hello CSS Modules</h1>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
